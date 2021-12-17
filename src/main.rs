8a53c5675ff59f034482d3eac485a988bb68f673
Hyeonmin Seon <hyunmui@outlook.kr>
2021-12-17 23:49:28 +0900

    게임 완성
    - 맞추는 부분 루핑 구문 추가 (맞출 때 까지 반복)

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,36 @@ use rand::Rng;
 fn main() {
     println!("숫자를 맞혀봅시다!");
 
+    // 번호 추출
     let secret_number = rand::thread_rng().gen_range(1..101);
-
     println!("사용자가 맞혀야 할 숫자: {}", secret_number);
 
-    println!("정답이라고 생각하는 숫자를 입력하세요");
+    // 실제 게임 부분
+    loop {
+        println!("정답이라고 생각하는 숫자를 입력하세요");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("입력한 값을 읽지 못했습니다.");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("입력한 값을 읽지 못했습니다.");
 
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("입력한 값이 올바른 숫자가 아닙니다.");
+        // 형변환 및 변환 실패 로직
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("입력한 값: {}", guess);
+        println!("입력한 값: {}", guess);
 
-    // 비교 결과
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("입력한 숫자가 작습니다!"),
-        Ordering::Greater => println!("입력한 숫자가 큽니다!"),
-        Ordering::Equal => println!("정답!"),
+        // 비교 결과
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("입력한 숫자가 작습니다!"),
+            Ordering::Greater => println!("입력한 숫자가 큽니다!"),
+            Ordering::Equal => {
+                println!("정답!");
+                break;
+            }
+        }
     }
 }
